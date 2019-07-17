3c9f5047d2fc2a48dba8b45d38f2a2a5f04e11fd
Roman Gardukevich <roman@gardukevich.com>
2019-07-17 22:06:26 +0300

    Initial Commit

---
+++ CoreDataPreserveMappingModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataPreserveMappingModel
+//
+//  Created by Roman Gardukevich on 17/07/2019.
+//  Copyright © 2019 Roman Gardukevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
